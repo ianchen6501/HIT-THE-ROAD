85594e15a40f8834f61a636ef9670ba9219fefb8
unknown <ianchen6501@gmail.com>
2020-12-14 15:11:45 +0800

    master demo

--- src/index.js
+++ src/index.js
@@ -3,11 +3,49 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from 'styled-components';
+
+const theme = {
+  primaryColors : {
+    primaryLighter : '#00000',
+    primaryLight : '#00000',
+    primaryMedium : '#00000',
+    primaryDark : '#00000',
+    primaryDarker : '#00000',
+    white: '#00000',
+    black: '#00000',
+  },
+
+  fontSizes : {
+    extraLarge : '36px',
+    Large: '30px',
+    medium: '26px',
+    Small: '20px',
+    extraSmall: '16px',
+  },
+
+  titles : {
+    h1 : '4.5rem',
+    h2 : '3.5rem',
+    h3 : '2.3rem',
+    h4 : '1.8rem',
+    h5 : '1.6rem',
+    h6 : '1.3rem',
+  },
+
+  Wrappers : {
+    maxWidth: '80vw',
+    mediumWidth: '70vw',
+  }
+
+}
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ThemeProvider theme={theme}>
     <App />
-  </React.StrictMode>,
+  </ThemeProvider>,
   document.getElementById('root')
 );
 
